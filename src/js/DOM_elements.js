cdab497e6387cce7af4359f8f9686858e48e1e86
BiAniTy <drayttc@gmail.com>
2022-05-22 12:59:22 -0300

    Creates module for DOM elements

---
+++ src/js/DOM_elements.js
@@ -0,0 +1,37 @@
+const DOM_el = (function () {
+    const main_container = document.querySelector(".main_container");
+
+    const weather_info = document.querySelector(".weather_info");
+
+    const name = document.querySelector(".name");
+    const main_info = document.querySelector(".main_info");
+    const temperature = document.querySelector(".temperature");
+    const humidity = document.querySelector(".humidity");
+    const min_max = document.querySelector(".min_max");
+    const min_temp = document.querySelector(".min_temp");
+    const max_temp = document.querySelector(".max_temp");
+    const description = document.querySelector(".description");
+
+    const extra_info = document.querySelector(".extra_info");
+
+    const pressure = document.querySelector(".pressure");
+    const wind_speed = document.querySelector(".wind_speed");
+
+    return {
+        main_container,
+        weather_info,
+        extra_info,
+        name,
+        main_info,
+        temperature,
+        humidity,
+        min_max,
+        min_temp,
+        max_temp,
+        description,
+        pressure,
+        wind_speed,
+    };
+})();
+
+export { DOM_el };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import "../css/weather.css";
+import { DOM_el } from "./DOM_elements";
 import { Weather } from "./weather_data";
 
 const init = () => {
